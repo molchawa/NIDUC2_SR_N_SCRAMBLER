079b9f917a1afcf105a17e98bf06b834cddb0a24
CaptainYunti <n.pawcio@gmail.com>
2018-06-09 21:40:43 +0200

    Teraz działa multi

--- codingMulti.m
+++ codingMulti.m
@@ -1,18 +1,16 @@
 
 function newData = codingMulti (data)
 
-  key = [1 0 0 1 0 1 0 1 0 0 0 0 0 0 0 1 0 0 1 0 1 0 1]; 
+  key = [1 0 0 1 0 0 0 1 0 0 1 0 0 0 0 1 0 0 0 0 0 0 1]; 
   indexKey = 1;
   indexData = 1;
-    while indexData<=length(data)
+  while indexData<=length(data)
       xoredBit = xor(key(18), key(23));
       xoredBit = xor(xoredBit, data(indexData));
       newData(indexData) = xoredBit;
       indexData = indexData+1;
-      for i = 0:21
-        key(23 - i) = key(22 - i);  
+      for i = 22:1
+        key(i+1) = key(i);  
       end
       key(1) = xoredBit;
     end
-      
-end
--- decodingMulti.m
+++ decodingMulti.m
@@ -1,15 +1,15 @@
 
 function newData = decodingMulti (data)
 
-  key = [1 0 0 1 0 1 0 1 0 0 0 0 0 0 0 1 0 0 1 0 1 0 1]; 
+  key = [1 0 0 1 0 0 0 1 0 0 1 0 0 0 0 1 0 0 0 0 0 0 1]; 
   indexKey = 1;
   indexData = 1;
     while indexData<=length(data)
       xoredBit = xor(key(18), key(23));
       xoredBit = xor(xoredBit, data(indexData));
       newData(indexData) = xoredBit;
-      for i = 0:21
-        key(23 - i) = key(22 - i);  
+      for i = 22:1
+        key(i+1) = key(i);  
       end
       key(1) = data(indexData);
       indexData = indexData+1;
